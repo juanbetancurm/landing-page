5e31623b14de587542ddb7dfbebefe7ed0857030
Juan Betancur <juan.jbetancur852@gmail.com>
2026-08-12 17:03:28 -0500

    Replace arrow animation to 180 rotation

--- code.bash
+++ code.bash
@@ -4,7 +4,7 @@ npm run build
 git status --short
 git add .
 git diff --cached --check
-git commit -m "Replace text arrows with shared icon"
+git commit -m "Replace arrow animation to 180 rotation"
 git pull --rebase origin main
 git push origin main
 
